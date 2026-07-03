42c57159fba4f87f9799690e4c176fd8bcc976e8
Landon Emiliano Hernadez Bonilla <landonemilianoh@gmai.com>
2026-07-03 00:31:55 -0600

    implementacion de la pregunta del monto

--- src/merceria/Merceria.java
+++ src/merceria/Merceria.java
@@ -17,38 +17,37 @@ public class Merceria {
         double montoTotal;
         double total;
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("-----------Merceria mayoritas------------");
-        
+
         System.out.println("¿Cuantos paquetes desea comprar?");
         cant = scan.nextInt();
-        
-        System.out.println("Ingrese el monto de la compra:");
-        scan = new Scanner(System.in);
-        montoTotal=scan.nextDouble();
-        
-        
+
+        // se evalua si el caso es posible o no
         if (cant < 5) {
             if (cant < 0) {
                 System.out.println("No se admiten compras negativas");
             }
             System.out.println("No se puede hacer esta compra ya que el minimo es de 5 paquetes");
         } else {
+            System.out.println("Ingrese el monto de la compra:");
+            scan = new Scanner(System.in);
+            montoTotal = scan.nextDouble();
             scan.nextLine();
             System.out.println("Cual fue el total de la compra");
             total = scan.nextDouble();
-            if (total<100) {
+            if (total < 100) {
                 double faltante = 100 - total;
-                System.out.println("No tienes descuento, le faltan "+faltante+ " para poder tener una promocion");
-                
+                System.out.println("No tienes descuento, le faltan " + faltante + " para poder tener una promocion");
+
             }
-            
+
             if (cant >= 5 && cant <= 10) {
                 System.out.println("El costo del envio es de 10 dolares");
-                if (total>100 && total < 300) {
+                if (total > 100 && total < 300) {
                     double promocion1 = total * 0.05;
-                    double pagoTotal= total + 10 - promocion1;
-                    System.out.println("Al comprar 100 tiene un descuento del 5 porciento, asi que el total con envio seria de "+ pagoTotal);
+                    double pagoTotal = total + 10 - promocion1;
+                    System.out.println("Al comprar 100 tiene un descuento del 5 porciento, asi que el total con envio seria de " + pagoTotal);
                 }
             } else {
                 System.out.println("El envio es gratis");
